cb8707fa6a9e7ae9e2ce6847d359dcd718b447c9
hardwaremc <167475971+hardwaremc@users.noreply.github.com>
2024-05-02 21:30:36 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+paises = []
+
+pais = input("Lemão, que páis você quer conhecer? ")
+paises.append(pais)
+
+paises.append(input("Qual o segundo país?"))
+paises.append(input("Qual o terceiro país?"))
+print("Minha lista de países é: ",paises)
